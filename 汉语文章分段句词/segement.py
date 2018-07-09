4a6bfd4fe0bd13a277b008694acc58634f4fa1d7
CHARLOTTE\Charlotte <Charlotte600@163.com>
2018-07-09 19:48:39 +0800

    汉语文章分词句段

---
+++ 汉语文章分段句词/segement.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Charlotte
+"""
+
+import jieba
+from snownlp import SnowNLP
+
+text=""
+
+f = open('corpus.txt','r')
+text=f.read()
+
+def cutparagraph(text):
+    paragraphslist = list()
+    print('以下是自行编写分段结果：\n')
+    for paragraph in text.split('\n\n'):
+        paragraphslist.append(paragraph)
+        print(paragraph + '\n' +'-'*50)
+    return paragraphslist
+
+def cutsentence(text):
+    sentenceslist = list()
+    s = SnowNLP(text)
+    sentenceslist = s.sentences
+    print('\n以下是调用snownlp分句的结果：\n')
+    for sentence in sentenceslist:
+        print(sentence + '\n' +'-'*50)
+    return sentenceslist
+
+def cutword(text):
+    wordslist = list()
+    wordslist = jieba.cut(text, cut_all=True)
+    print('\n以下是调用结巴分词的结果：\n')
+    print(wordslist)
+    return wordslist
+
+cutparagraph(text)
+cutsentence(text)
+cutword(text)
